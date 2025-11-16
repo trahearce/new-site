8ec3dc1ffbf0417cf5e85f5ec075c61e4d97ee24
gorio-7 <black.kz717@gmail.com>
2025-11-16 20:22:46 +0900

    Add files via upload

--- item.js
+++ item.js
@@ -2,3 +2,19 @@ function ChangeImage(subImage) {
   const main = document.getElementById("mainImage");
   main.src = subImage.src;
 }
+
+const minusBtn = document.querySelector(".minus");
+const plusBtn = document.querySelector(".plus");
+const countInput = document.querySelector(".count");
+
+minusBtn.addEventListener("click", () => {
+  let value = parseInt(countInput.value);
+  if (value > 1) { // 最低1個
+    countInput.value = value - 1;
+  }
+});
+
+plusBtn.addEventListener("click", () => {
+  let value = parseInt(countInput.value);
+  countInput.value = value + 1;
+});
